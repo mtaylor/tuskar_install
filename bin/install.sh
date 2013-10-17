641d7e98b6d0c7885d3b3d9704e40db2a06d4df8
Martyn Taylor <mtaylor@redhat.com>
2013-10-17 13:31:44 +0100

    Added more install

--- bin/install.sh
+++ bin/install.sh
@@ -1,18 +1,21 @@
-UNDERCLOUDRC=/etc/sysconfig/undercloudrc
+UNDERCLOUDRC=/root/stackrc
 INSTALL_DIR=/opt/stack
 NOVA_INSTALL_DIR=/opt/stack/venvs/nova/lib/python2.7/site-packages/nova
+BASE_DIR=`dirname $0`/../
 
 # Install deps
-sudo yum -y install python-devel swig openssl-devel python-pip mysql-devel libxml2-devel libxslt-devel tox
+sudo yum -y install python-devel swig openssl-devel python-pip mysql-devel libxml2-devel libxslt-devel tox patch
 sudo easy_install nose
 sudo pip install virtualenv setuptools-git flake8 tox
 
 # Patch Nova - Requires a Nova Restart
-cd $NOVA_INSTALL_DIR
-git init
+cd $BASE_DIR/patches/nova
+sudo find $NOVA_INSTALL_DIR -name "*.pyc" -exec rm -rf {} \;
+sudo find . -name "*.*" -exec patch $NOVA_INSTALL_DIR/{} {} \;
+sudo service nova-api restart
 
-# Source credentials and change install dir
-source $UNDERCLOUDRC
+# Source credentials
+sudo source $UNDERCLOUDRC
 
 # Setup Tuskar Core
 cd $INSTALL_DIR
@@ -21,6 +24,7 @@ cd tuskar
 
 ## Configure Tuskar Core
 cp etc/tuskar/tuskar.conf.sample etc/tuskar/tuskar.conf
+
 echo "
 # Tuskar Core Credentials
 username=$OS_USERNAME
@@ -34,10 +38,32 @@ insecure=True
 tox -evenv -- echo 'done'
 source .tox/venv/bin/activate
 pip install -r requirements.txt
-pip install -r test-requirements.txt
+tuskar-dbsync --config-file etc/tuskar/tuskar.conf
+
+# Run Tuskar API service
+mkdir $INSTALL_DIR/tuskar/log/
+nohup tuskar-api --config-file etc/tuskar/tuskar.conf --debug > $INSTALL_DIR/tuskar/log/api.log 2>&1 &
 
 # Setup Tuskar UI
 
 cd $INSTALL_DIR
+git clone git://github.com/openstack/horizon.git
 git clone https://github.com/openstack/tuskar-ui
 
+# Create symbolic link to tuskar-ui
+cd horizon
+ln -s ../tuskar-ui/tuskar_ui
+
+# Install deps following tuskar-ui readme steps
+cp ../tuskar-ui/local_settings.py.example openstack_dashboard/local/local_settings.py
+python tools/install_venv.py
+source .venv/bin/activate
+sudo pip install git+http://github.com/openstack/python-tuskarclient.git
+
+#./run_tests.sh
+
+# Open up Port for remote access
+sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+
+mkdir $INSTALL_DIR/horizon/log
+nohup tools/with_venv.sh ./manage.py runserver 0.0.0.0:8080 > $INSTALL_DIR/horizon/log/horizon.log 2>&1 &
